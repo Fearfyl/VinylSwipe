f7c3e7751d142fdb467dc241a1b4c8b032fb0dfe
Fearfyl <wadebuchanan18@gmail.com>
2025-10-01 19:15:55 -0400

    Refactor code structure for improved readability and maintainability

--- server/server.js
+++ server/server.js
@@ -4,32 +4,17 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import recommendationRoutes from "./routes/recommendationRoutes.js";
 
-import swipeRoutes from "./routes/swipeRoutes.js";
-
-
-import swipeRoutes from "./routes/swipeRoutes.js";
-
-
 dotenv.config();
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// DB
+// connect DB
 connectDB();
 
-// Routes
+// routes
 app.use("/api/users", userRoutes);
 app.use("/api/recommendations", recommendationRoutes);
 
-app.use("/api/swipes", swipeRoutes);
-
-
-app.use("/api/swipes", swipeRoutes);
-
-
-
-// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
